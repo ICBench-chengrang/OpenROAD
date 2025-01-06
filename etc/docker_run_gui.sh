c6e38d7d4363c6580471d0bbbe9a39f9d826ac36
ICBench-chengrang <chengrang@icbench.com>
2025-01-06 18:01:57 +0800

    add docker_run script

--- etc/docker_run_gui.sh
+++ etc/docker_run_gui.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 
 image=openroad/flow-ubuntu22.04-builder:latest
-
 docker run --rm -it \
 	-u $(id -u ${USER}):$(id -g ${USER}) \
 	-v $(pwd)/flow:/OpenROAD-flow-scripts/flow \
+	-e DISPLAY=${DISPLAY} \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-v ${HOME}/.Xauthority:/.Xauthority \
+	--network host \
+	--security-opt seccomp=unconfined \
 	$image
